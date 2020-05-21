cb153e2039043f7482dc9ead82fc557516104548
Laura <lstorella10@gmail.com>
2020-05-22 00:08:11 +0200

    modifica js - html - css

--- js/script.js
+++ js/script.js
@@ -1,72 +1,79 @@
-// Start Program
+// ELEMENTI FORM
+// Dati passeggero
+var formNome = document.getElementById('form-nome');  //Nome
+var formKm = document.getElementById('form-tratta'); //Km
+var formEta = document.getElementById('form-eta'); //Km
 
-// DATI FORM
-// Richiesta Dati Passeggero
-var passengerName = document.getElementById('form-nome');
-console.log(passengerName);
+// Buttons
+var btnGenera = document.getElementById('form-genera');
+var btnAnnulla = document.getElementById('form-annulla');
 
-
-var passengerKm = document.getElementById('form-tratta');
-console.log(passengerKm);
-
-var passengerAge = document.getElementById('form-eta');
-console.log(passengerAge);
 
 // Altre Variabili
-
-
-// Elementi Biglietto
-var biglNome = document.getElementById('ticket-nome');
-
+var formNomeValue;
+var formKmValue;
+var formEtaValue;
 
 // EVENTI BUTTONS
-// Btn Genera
-var btnGenera = document.getElementById('cta-genera');
+// BTN GENERA
+var btnGenera = document.getElementById('form-genera');
 btnGenera.addEventListener('click',
   function() {
-    var passengerName = document.getElementById('form-nome').value;
-    biglNome.innerHTML = passengerName;
+    // Elabora elementi Biglietto
+    formNomeValue = formNome.value;
+    biglNome.innerHTML = formNomeValue;
+    biglCosto.innerHTML = prezzo + ' Euro';
+    // Elabora Km
+    formKmValue = formKm.value;
+    // Elabora eta
+    formEtaValue = formEta.value;
+
+    // Calcolo prezzo
+    var prezzo = formKmValue * 0.21;
+
+    // Calcolo sconti
+    if (formEtaValue == 'min') {
+      prezzo = prezzo - (prezzo * 20 / 100);
+    } else if (formEtaValue == 'over') {
+      prezzo = prezzo - (prezzo * 40 / 100);
+    }
   }
 )
 
-// Btn Annulla
-var btnAnnulla = document.getElementById('cta-annulla');
+// BTN ANNULLA
+var btnAnnulla = document.getElementById('form-annulla');
 btnAnnulla.addEventListener('click',
   function() {
-    var passengerName = document.getElementById('form-nome').value = '';
-    biglNome.innerHTML = passengerName.value = '';
+    // Annulla Nome
+    formNome.value = '';
+    biglNome.innerHTML = '';
+
+    // Annulla Km
+    formKm.value = '';
+
+    // Annulla Eta
+    formEta.value = 'mag'
+
+    // Hide ticket
+    var ticketPrev = document.getElementById('ticket-preview');
+    ticketPrev.className = 'hidden';
   }
 )
 
 
+// ELEMENTI BIGLIETTO
+var biglNome = document.getElementById('ticket-nome');
+var biglCosto = document.getElementById('ticket-costo');
 
 
+//ELEMENTI PRINTABLE TICKET
+var btnPrint= document.getElementById('btn-print');
+btnPrint.addEventListener('click',
+  function() {
 
-// CALCOLO BIGLIETTO
-// Costo biglietto intero - Prezzo definito in base ai km (0.21 € al km)
-var fullPriceTicket = Math.round( (passengerKm * 0.21) * 100 ) / 100;
-console.log(fullPriceTicket);
-document.getElementById('prezzo-pieno').innerHTML = 'Il prezzo pieno del biglietto è di € ' + fullPriceTicket;
+    // Hide / change ticket name
+    var printName = document.getElementById('pass-id');
+    printName.className = 'hidden';
 
-// Sconto biglietto 20% per under 18
-var ticketDiscount20 = Math.round( (fullPriceTicket - (fullPriceTicket * 20 / 100)) * 100 ) / 100;
-
-// Sconto biglietto 40% per over 65
-var ticketDiscount40 = Math.round( (fullPriceTicket - (fullPriceTicket * 40 / 100)) * 100 ) / 100;
-
-if (passengerAge < 18) {
-  console.log('Il costo del tuo biglietto è di € ' + ticketDiscount20 );
-  document.getElementById('sconto20').innerHTML = 'Il costo del tuo biglietto è di € ' + ticketDiscount20;
-} else if (passengerAge > 65) {
-  console.log('Il costo del tuo biglietto è di € ' + ticketDiscount40);
-  document.getElementById('sconto40').innerHTML = 'Il costo del tuo biglietto è di € ' + ticketDiscount40
-} else {
-  console.log('Il costo del tuo biglietto è di € ' + fullPriceTicket);
-  document.getElementById('no-sconto').innerHTML = 'Il costo del tuo biglietto è di € ' + fullPriceTicket
-  document.getElementById('sconto20').innerHTML = 'Sconto non applicabile';
-  document.getElementById('sconto40').innerHTML = 'Sconto non applicabile';
-}
-
-alert('Procedi alla stampa del tuo biglietto e...buon viaggio!');
-
-// End Program
+  }
+)
