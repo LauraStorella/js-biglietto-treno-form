38c5c9645befc6c4e352e2a5cd1f6b720603331b
Laura <lstorella10@gmail.com>
2020-05-22 15:35:18 +0200

    modifica js

--- js/script.js
+++ js/script.js
@@ -1,81 +1,76 @@
 // ELEMENTI FORM
-// Dati passeggero
-var formNome = document.getElementById('form-nome');  //Nome
-var formKm = document.getElementById('form-tratta'); //Km
-var formEta = document.getElementById('form-eta');  //Eta
+// ELEMENTI INPUT FORM
+var formNome = document.getElementById('form-nome');
+var formKm = document.getElementById('form-tratta');
+var formEta = document.getElementById('form-eta');
+var prezzo = formKm.value * 0.21;
 
-// Buttons
-var btnGenera = document.getElementById('form-genera');
-var btnAnnulla = document.getElementById('form-annulla');
-
-
-// Altre Variabili
-var formNomeValue;
-var formKmValue;
-var formEtaValue;
+// ELEMENTI BUTTONS
+var formGenera = document.getElementById('form-genera');
+var formAnnulla = document.getElementById('form-annulla');
 
 
 // ELEMENTI BIGLIETTO
 var biglNome = document.getElementById('ticket-nome');
 var biglCosto = document.getElementById('ticket-costo');
+var biglOfferta = document.getElementById('ticket-offerta');
+var biglCarrozza = document.getElementById('ticket-carrozza');
+var biglCodiceCp = document.getElementById('ticket-codice');
 
 
-// EVENTI BUTTONS
-// BTN GENERA
-var btnGenera = document.getElementById('form-genera');
-btnGenera.addEventListener('click',
+// Eventi Click Genera
+formGenera.addEventListener('click',
   function() {
-    // Elabora elementi Biglietto
-    formNomeValue = formNome.value;
-    biglNome.innerHTML = formNomeValue;
-    biglCosto.innerHTML = prezzo.toFixed(2) + ' Euro';
-    // Elabora Km
-    formKmValue = formKm.value;
-    console.log(formKmValue);
-    // Elabora eta
-    formEtaValue = formEta.value;
-    console.log(formEtaValue);
 
-    // Calcolo prezzo
-    var prezzo = formKmValue * 0.21;
+
+    // Calcolo costo biglietto intero
+    var prezzo = formKm.value * 0.21;
+    offerta = 'Prezzo standard';
     console.log(prezzo);
 
-    // Calcolo sconti
-    if (formEtaValue == 'min') {
-      prezzo = prezzo - (prezzo * 20 / 100);
-    } else if (formEtaValue == 'over') {
-      prezzo = prezzo - (prezzo * 40 / 100);
+    // Calcolo eventuali sconti
+    if (formEta.value == 'min') {
+      prezzo = prezzo - ( prezzo * 20 / 100);
+      var offerta = 'Sconto minorenni';
+    } else if (formEta.value == 'over') {
+      prezzo = prezzo - ( prezzo * 40 / 100);
+      var offerta = 'Sconto over 65';
     }
 
+    // Genera n° Carrozza
+    var carrozza = Math.floor(Math.random() * 10) + 1;
+
+    // Genera CodiceCP
+    var codiceCp = Math.floor(Math.random() * 1000) + 1;
+
+
+    // Genera contenuto biglietto
+    biglNome.innerHTML = formNome.value; //Nome passeggero
+    biglCosto.innerHTML = prezzo.toFixed(2) + ' Euro'; //Costo biglietto
+    biglOfferta.innerHTML = offerta; //Tipologia offerta
+    biglCarrozza.innerHTML = carrozza; //N° carrozza
+    biglCodiceCp.innerHTML = codiceCp; //N° codice CP
   }
 )
 
-// BTN ANNULLA
-var btnAnnulla = document.getElementById('form-annulla');
-btnAnnulla.addEventListener('click',
+
+// Eventi Click Annulla
+formAnnulla.addEventListener('click',
   function() {
-    // Annulla Nome
+    // Reset valori form
     formNome.value = '';
-    biglNome.innerHTML = '';
-
-    // Annulla Km
     formKm.value = '';
+    formEta.value = 'mag';
 
-    // Annulla Eta
-    formEta.value = 'mag'
+    // Reset valori biglietto
+    biglNome.innerHTML = '';
+    biglCosto.innerHTML = '';
 
     // Hide ticket
     var ticketPrev = document.getElementById('ticket-preview');
     ticketPrev.className = 'hidden';
   }
 )
-
-
-
-
-
-
-
 
 
 
